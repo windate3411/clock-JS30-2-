bfbfc3385f4f13b77aea3a41673ec2217ef5de2c
Danny <windate3411@gmail.com>
2019-05-23 20:48:06 +0800

    刪除不必要之空白

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const secondHand = document.querySelector('.second-hand')
-const minHand = document.querySelector('.min-hand')
-const hourHand = document.querySelector('.hour-hand')
-
-function setDate() {
+(function setDate() {
+  const secondHand = document.querySelector('.second-hand')
+  const minHand = document.querySelector('.min-hand')
+  const hourHand = document.querySelector('.hour-hand')
   //建立Date物件取得現在時間
   const now = new Date()
   //取得現在秒數與對應的角度，度數+90是因為原先時針是指向9點鐘方向
@@ -18,6 +17,6 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondDegrees}deg)`
   minHand.style.transform = `rotate(${minuteDegrees}deg)`
   hourHand.style.transform = `rotate(${hourDegrees}deg)`
-}
-//每一千毫秒執行一次程式
-setInterval(setDate, 1000);
+  //每一千毫秒執行一次程式
+  setInterval(setDate, 1000);
+})();
